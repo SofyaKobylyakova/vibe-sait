719e21acf0311fc246b7672373ddda4bea8253e6
Sofya Kobylyakova <sofya.kobylyakova@mail.ru>
2023-02-12 16:00:44 +0300

    fixed header adaptive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const NavBtn=document.querySelector(".menu-burger")
 const Menu=document.querySelector("nav")
+const Tel=document.querySelector(".header-tel")
 NavBtn.onclick = function() {
     NavBtn.classList.toggle('menu-on')
     Menu.classList.toggle('nav-visible')
+    Tel.classList.toggle('tel-off')
 }
 
